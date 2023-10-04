75a0d60eb07e4899b4e1367d0a6b9c05ef7e8fbb
aamir <aamirp1@outlook.com>
2023-10-04 14:58:46 +0100

    complete webscraper script

--- main.py
+++ main.py
@@ -1,37 +1,90 @@
+import json
 from selenium import webdriver
+from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(options=options)
-
-def initialize_browser():
-    driver.get('https://www.tapology.com/fightcenter/events/101866-ufc-fight-night')
-    print("starting_Driver")
-
-    # Corrected method to find elements
-    fight_card_bouts = driver.find_elements(By.CSS_SELECTOR, ".fightCardFighterName.left")
-    fight_card_bouts_two = driver.find_elements(By.CSS_SELECTOR, ".fightCardFighterName.right")
+BASE_URL = 'https://www.tapology.com'
+MAJOR_ORGS = ['UFC', 'PFL', 'BELLATOR', 'ONE', 'RIZIN']
 
 
-    for bout in fight_card_bouts:
-        try:
-            # Using the method to find the nested anchor element and get its text
-            fight_name_left = bout.find_element(By.CSS_SELECTOR, "a").text
-            print(fight_name_left)
-        except Exception as e:
-            print(f'error: {e}')  # Log the error and continue
-    for bout in fight_card_bouts_two:
-        try:
-            # Using the method to find the nested anchor element and get its text
-            fight_name_right = bout.find_element(By.CSS_SELECTOR, "a").text
-            print(fight_name_right)
-        except Exception as e:
-            print(f'error: {e}')  # Log the error and continue
-    driver.quit()
+def get_browser():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    return webdriver.Chrome(options=options)
 
-initialize_browser()
+
+def extract_event_details(el):
+    title_element = el.select_one('span.name a')
+    title = title_element.text.strip() if title_element else None
+
+    datetime_element = el.select_one('span.datetime')
+    date = datetime_element.text.strip() if datetime_element else None
+
+    link = BASE_URL + title_element['href'] if title_element else None
+
+    return {"title": title, "date": date, "link": link}
+
+
+def filter_major_orgs(events):
+    return [event for event in events if any(org in event["title"].upper() for org in MAJOR_ORGS)][:10]
+
+
+def extract_fight_details(el):
+    def get_weight():
+        weight_element = el.select_one('.weight')
+        return weight_element.text if weight_element else None
+
+    def get_fighter(side):
+        name_selector = f'.fightCardFighterName.{side} a'
+        link_selector = f'.fightCardFighterBout.{side} a'
+        rank_selector = f'.fightCardFighterRankNum.{side}.world .number'
+        record_selector = f'.fightCardRecord'
+
+        name = el.select_one(name_selector).text
+        link = BASE_URL + el.select_one(link_selector)['href']
+        rank = el.select_one(rank_selector).nextSibling.strip(
+        ) if el.select_one(rank_selector) else None
+        record = el.select_one(record_selector).text.strip(
+        ) if el.select_one(record_selector) else None
+
+        return {"name": name, "link": link, "rank": rank, "record": record}
+
+    main_card = 'main' in el.select_one('.billing').text.lower()
+    title_match = el.select_one('.fightCardWeight span.title') is not None
+
+    return {
+        "title_match": title_match,
+        "main_card": main_card,
+        "weight": get_weight(),
+        "fighterA": get_fighter('left'),
+        "fighterB": get_fighter('right')
+    }
+
+
+def scrape():
+    browser = get_browser()
+    browser.get(f"{BASE_URL}/fightcenter?group=major&schedule=upcoming")
+    soup = BeautifulSoup(browser.page_source, 'lxml')
+
+    events = [extract_event_details(el) for el in soup.select('.promotion')]
+    events = filter_major_orgs(events)
+
+    for event in events:
+        browser.get(event['link'])
+        soup = BeautifulSoup(browser.page_source, 'lxml')
+        event["fights"] = [extract_fight_details(
+            el) for el in soup.select('li.fightCard:not(.picks)')]
+
+    browser.quit()
+
+    return [event for event in events if len(event["fights"]) > 4]
+
+
+if __name__ == "__main__":
+    scraped_data = scrape()
+
+    with open('output.json', 'w') as f:
+        json.dump(scraped_data, f, indent=4)
