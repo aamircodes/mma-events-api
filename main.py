94b4fe7607272aa79d711c8cac3862a382d620d4
aamir <aamirp1@outlook.com>
2023-10-09 12:40:51 +0100

    heroku error fix

--- main.py
+++ main.py
@@ -33,13 +33,20 @@ def store_data(data):
 
 
 def get_browser():
-    options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    return webdriver.Chrome(options=options)
+    # options = Options()
+    # options.add_argument('--headless')
+    # options.add_argument('--disable-gpu')
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-dev-shm-usage')
+    # options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    driver = webdriver.Chrome(executable_path=os.environ.get(
+        "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    return driver
 
 
 def filter_major_orgs(events):
